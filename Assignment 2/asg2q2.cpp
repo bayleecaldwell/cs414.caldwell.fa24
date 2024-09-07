bf1198992f44d3c982457c8af7c6a0c2ca2ff7bc
baylee <bcaldwel@my.athens.edu>
2024-09-06 19:17:25 -0500

    Update asg2q2.cpp

--- Assignment 2/asg2q2.cpp
+++ Assignment 2/asg2q2.cpp
@@ -1,1 +1,24 @@
+#include <functional>
+#include <unordered_map>
+
+class CaseOf {
+public:
+    template<typename T>
+    CaseOf(T value, const std::unordered_map<T, std::function<std::string()>>& cases, const std::function<std::string()>& defaultCase)
+        : value_(value), cases_(cases), defaultCase_(defaultCase) {}
+
+    std::string evaluate() const {
+        auto it = cases_.find(value_);
+        if (it != cases_.end()) {
+            return it->second();
+        } else {
+            return defaultCase_();
+        }
+    }
+
+private:
+    const int value_;
+    const std::unordered_map<int, std::function<std::string()>>& cases_;
+    const std::function<std::string()>& defaultCase_;
+};
 
